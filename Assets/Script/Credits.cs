fe7108e3595afbb11210b2b65ec486d3d5b3f8a8
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2023-12-29 16:31:25 -0500

    Day 29

--- Assets/Script/Credits.cs
+++ Assets/Script/Credits.cs
@@ -1,4 +1,8 @@
-// Art - CraftPixel.net
+//Level 1 Title Palette - CraftPixel.net
+//Level 1 NPCs - CraftPixel.net
+//Level 2 Title Palette - Luis Zuno (@ansimuz)
+//Level 2 Coins - Simple 2D Platformer Assets Pack | Goldmetal
+//Level 2 NPCs - CraftPixel.net
 //Dialogue - HeneGame
 //Lead Programming - Stephen Shaw
 //Lead Story Writer - Stephen Shaw
